1f7e406f9573b699161537b0b006f19e4703f0e3
Bhushan <bhushan1810patil@gmail.com>
2021-04-28 14:17:09 +0530

    added basic class structure

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
+class Instagram(object):
+
+    def __init__(self):
+        self.driver = None
+        self.chrome_options = None
+        self.response = None
+        self.chrome_path = os.path.abspath("chromedriver.exe")
+        self.wait = None
+        self.maxImgDownloads = 0
+        self.maxStoryDownloads = 0
+        self.stories = list()
+        self.images = list()
+        self.carousellist = list()
+
+    def login(self):
+        username = input("Input Username: ")
+        password = getpass(prompt="Enter Password: ")
+
+if __name__ == "__main__":
+    print("Hello, World")
